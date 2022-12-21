9667347faaee727006590a5921b27f6b71dcfebf
sharmavins23 <sharmavins23@gmail.com>
2022-12-21 17:05:57 -0500

    route loading is done

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log("Hello, World!");
+// The "require" statement looks up and caches a module. Basically, it "imports"
+// Why not "import"? "import" only works in a JavaScript "module", which this is
+//  not. JavaScript modules are overcomplicated to set up and we don't need it
+// This is a `const` because we don't want to be modifying any of the express
+//  code that was provided so kindly to us
+
+// Express is utilized to handle server networking and stuff for us.
+const express = require("express");
+// Morgan is utilized for logging the requests coming into our server in the
+//  console. It also pretty prints them with nice colors
+const morgan = require("morgan");
+
+// Our TCP/UDP port number. 
+// See: https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
+// We can *arbitrarily* use any port number, but make sure it doesn't collide
+//  with another port number in use.
+const port = 8008;
+
+// Let's create our application. Express handles the server structure, so we can
+//  tell express that we want an app:
+const app = express();
+// Now we can tell this app that we want to use JSON requests, I.E. we want to
+//  send and receive JSON data from the server. We can change this later
+app.use(express.json());
+// Let's also tell the app to print out any requests coming in to the console
+//  via pretty print from Morgan
+// The "dev" is an option, which basically tells it to run in verbose mode
+// Other options: https://expressjs.com/en/resources/middleware/morgan.html
+app.use(morgan("dev"));
+
+// Let's dynamically load in all of our routes. This takes the DLR.js file and
+//  handles everything for us
+require("./routes/DLR")(app);
+
+// Let's now start our server.
+// Once our server is running, NONE of the code beyond this line will run, since
+//  the server will spin constantly, waiting for requests.
+app.listen(port, () => {
+    console.log(`Server is up and running at http://localhost:${port}`);
+});
+
+// The () => {} is JavaScript arrow notation. This creates a function and passes
+//  it into the app.listen() parameters. We don't name this function, so it's
+//  called a "lambda" function (or an "anonymous") function. We don't name it
+//  because we simply don't want to (or care to)
+// The console.log() uses backticks ` in order to use the string. This allows
+//  us to put in JavaScript variables and code in the string, without adding the
+//  string together. It's equivalent to the following:
+// console.log("Server is up and running at http://localhost:" + port);
+//  It's just a little more concise.
---
+++ routes/DLR.js
@@ -0,0 +1,66 @@
+// When the server starts up, it can read in all of the JavaScript files in this
+//  folder, and import all of them
+
+// We'll use fs, or JavaScript's file system structure, to read through all of
+//  the code files in this folder. JavaScript can then import each and every
+//  one of the files so we don't have to do so manually!
+const fs = require("fs");
+
+// This is the function that will go through all of the JavaScript files in the
+//  folder, and import them individually. This saves us headache (as if we want
+//  to add in more routes later, we don't have to manually import them in the 
+//  index.js file.)
+function dynamicallyLoadRoutes(app) {
+    // Read through all of the files in this directory
+    // This returns an array of file NAMES (strings)
+    // readdirSync tells JavaScript that we want to read through them each
+    //  individually, and not all in parallel, because JavaScript likes doing
+    //  things in parallel and we don't want that.
+    // __dirname is a reserved keyword for the current directory name, so it'll
+    //  tell JavaScript that we want to read within /routes/...
+    let files = fs.readdirSync(__dirname);
+
+    // Now let's apply a function to each file. Remember the () => {} arrow
+    //  notation? This line says "hey, for each file, let's do this function"
+    // The file is passed in as a parameter for the function
+    files.forEach((file) => {
+        // Since this returns a list of files, that list of files will also have
+        //  DLR.js. We don't want to double import this file so we have to skip
+        //  it. While we're skipping, let's also skip anything that's not a 
+        //  JavaScript file
+        if (
+            // Triple equals makes JavaScript compare the strings more strictly
+            file === "DLR.js" ||
+            // This is a complicated string. Basically, we want to see what file
+            //  extension the file has (so "test.js" should return "js")
+            // We find the last index of ".", and move past the "." character.
+            //  Hence the + 1.
+            // We then find the substring, which takes in a start and optionally
+            //  a stop. Since we only pass one thing in, that's the start
+            //  So, the substring starts after the "." and ends at the end
+            file.substr(file.lastIndexOf(".") + 1) !== "js"
+        ) {
+            // If any of those conditions are met, we don't want to do anything
+            //  to this file. So, return from this small function
+            return;
+        }
+
+        // Let's instead use the substring to get the filename. Note that a file
+        //  is named "filename.js"; We've already skipped any non ".js" files.
+        // So, instead of skipping past the "." and getting the substring there,
+        //  let's instead get the start of it up to the "."
+        let filename = file.substr(0, file.indexOf("."));
+        // Now we're going to import the filename, similarly to how we did in 
+        //  the index.js file
+        // The "./" is because we're loading that in as a route. So if we visit
+        //  https://localhost:8008/test and we had a test.js file, it would work
+        // The (app) thing passes in the expressJS app into the file as a
+        //  parameter to a function
+        require("./" + filename)(app);
+    });
+}
+
+// This file is a "module", and we'll have the "module" "export" a function.
+// This function simply goes through all of the files in the /routes/ folder,
+//  reads their name, and imports them.
+module.exports = dynamicallyLoadRoutes;
