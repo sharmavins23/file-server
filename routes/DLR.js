d4652faef5cb1a2081edcf1f722a63076b251379
sharmavins23 <sharmavins23@gmail.com>
2022-12-22 16:43:09 -0500

    added WAY more docs

--- routes/DLR.js
+++ routes/DLR.js
@@ -18,19 +18,19 @@ function dynamicallyLoadRoutes(app) {
     //  things in parallel and we don't want that.
     // __dirname is a reserved keyword for the current directory name, so it'll
     //  tell JavaScript that we want to read within /routes/...
-    let files = fs.readdirSync(__dirname);
+    let filenames = fs.readdirSync(__dirname);
 
     // Now let's apply a function to each file. Remember the () => {} arrow
     //  notation? This line says "hey, for each file, let's do this function"
     // The file is passed in as a parameter for the function
-    files.forEach((file) => {
+    filenames.forEach((filename) => {
         // Since this returns a list of files, that list of files will also have
         //  DLR.js. We don't want to double import this file so we have to skip
         //  it. While we're skipping, let's also skip anything that's not a 
         //  JavaScript file
         if (
             // Triple equals makes JavaScript compare the strings more strictly
-            file === "DLR.js" ||
+            filename === "DLR.js" ||
             // This is a complicated string. Basically, we want to see what file
             //  extension the file has (so "test.js" should return "js")
             // We find the last index of ".", and move past the "." character.
@@ -38,7 +38,7 @@ function dynamicallyLoadRoutes(app) {
             // We then find the substring, which takes in a start and optionally
             //  a stop. Since we only pass one thing in, that's the start
             //  So, the substring starts after the "." and ends at the end
-            file.substr(file.lastIndexOf(".") + 1) !== "js"
+            filename.substr(filename.lastIndexOf(".") + 1) !== "js"
         ) {
             // If any of those conditions are met, we don't want to do anything
             //  to this file. So, return from this small function
@@ -49,14 +49,14 @@ function dynamicallyLoadRoutes(app) {
         //  is named "filename.js"; We've already skipped any non ".js" files.
         // So, instead of skipping past the "." and getting the substring there,
         //  let's instead get the start of it up to the "."
-        let filename = file.substr(0, file.indexOf("."));
+        let jsModule = filename.substr(0, filename.indexOf("."));
         // Now we're going to import the filename, similarly to how we did in 
         //  the index.js file
         // The "./" is because we're loading that in as a route. So if we visit
         //  https://localhost:8008/test and we had a test.js file, it would work
         // The (app) thing passes in the expressJS app into the file as a
         //  parameter to a function
-        require("./" + filename)(app);
+        require("./" + jsModule)(app);
     });
 }
 
